3d8be8593935c4f9c9f9396d2a33bf70a98a3b08
Carlos Murillo <murillo.carlos01@gmail.com>
2025-08-22 22:00:29 -0500

    Added mapping to find a cigar by id

--- src/main/java/dev/cmurillo/SmokeNotesBackend/Cigar/CigarController.java
+++ src/main/java/dev/cmurillo/SmokeNotesBackend/Cigar/CigarController.java
@@ -1,10 +1,12 @@
 package dev.cmurillo.SmokeNotesBackend.Cigar;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/cigars/")
@@ -21,4 +23,12 @@ public class CigarController {
         return cigarRepository.findAll();
     }
 
+    @GetMapping("{id}/")
+    Cigar findById(@PathVariable Integer id) {
+        Optional<Cigar> cigar = cigarRepository.findByCigarId(id);
+        if (cigar.isEmpty()) {
+            throw new CigarNotFoundException("Cigar with id: " + id + " , could not be found");
+        }
+        return cigar.get();
+    }
 }
--- src/main/java/dev/cmurillo/SmokeNotesBackend/Cigar/CigarRepository.java
+++ src/main/java/dev/cmurillo/SmokeNotesBackend/Cigar/CigarRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class CigarRepository {
@@ -24,5 +25,11 @@ public class CigarRepository {
                 .list();
     }
 
+    public Optional<Cigar> findByCigarId(Integer id) {
+        return jdbcClient.sql("select * from cigar where cigar_id = ?")
+                .param(id)
+                .query(Cigar.class)
+                .optional();
+    }
 
 }
