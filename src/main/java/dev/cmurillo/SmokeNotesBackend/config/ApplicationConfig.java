a78680338474b7d3c268f9ea1e04c1bf4efc10d1
Carlos Murillo Jr <carlosmurillojr@Carloss-MacBook-Pro.local>
2025-10-25 14:50:55 -0500

    Created a config package and added an ApplicationConfig to handle auth config

---
+++ src/main/java/dev/cmurillo/SmokeNotesBackend/config/ApplicationConfig.java
@@ -0,0 +1,48 @@
+package dev.cmurillo.SmokeNotesBackend.config;
+
+import dev.cmurillo.SmokeNotesBackend.Exceptions.UserNotFoundException;
+import dev.cmurillo.SmokeNotesBackend.Repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    public ApplicationConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // Create a bean so that we can find a user by their username (email) from our repo
+    @Bean
+    UserDetailsService userDetailsService() {
+        return username -> userRepository.findByUsername(username)
+                .orElseThrow(() -> new UserNotFoundException("A user with the email " + username + " was not found."));
+    }
+
+    // Creating a bean so that we can use a password encoder and not store user passwords in plain text
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    // Bean so that we can use our Dao Auth Provider with the provided userDetailService and our password encoder
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService());
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder());
+        return authProvider;
+    }
+}
