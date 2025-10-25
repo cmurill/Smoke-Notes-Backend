51f798192ee82e88f11a5e91ae00afe0ffa03922
Carlos Murillo Jr <carlosmurillojr@Carloss-MacBook-Pro.local>
2025-10-25 16:16:17 -0500

    Added a Security Config class for the security filter chain

---
+++ src/main/java/dev/cmurillo/SmokeNotesBackend/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package dev.cmurillo.SmokeNotesBackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final AuthenticationProvider authProvider;
+    private final JwtAuthenticationFilter jwtAuthFilter;
+
+    public SecurityConfig(AuthenticationProvider authProvider, JwtAuthenticationFilter jwtAuthFilter) {
+        this.authProvider = authProvider;
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
